be4275b92258f56ccbe6d95450f0f832c744acb8
Alexander Radchenko <phejet@gmail.com>
2025-06-09 00:39:52 +1000

    testing

--- flake.nix
+++ flake.nix
@@ -5,5 +5,14 @@
   outputs = { self, nixpkgs }: {
     packages.x86_64-linux.mylib = nixpkgs.legacyPackages.x86_64-linux.callPackage ./default.nix {};
     defaultPackage.x86_64-linux = self.packages.x86_64-linux.mylib;
+
+    devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
+      buildInputs = [
+        nixpkgs.legacyPackages.x86_64-linux.cmake
+        nixpkgs.legacyPackages.x86_64-linux.ninja
+        nixpkgs.legacyPackages.x86_64-linux.gcc
+        nixpkgs.legacyPackages.x86_64-linux.clang-tools
+      ];
+    };
   };
 }
--- tests/test_add.cpp
+++ tests/test_add.cpp
@@ -1,10 +1,13 @@
-#include <cassert>
+#include <assert.h>
+#include <stdio.h>
 #include "mylib/mylib.h"
 
 int main() {
     // Basic test for add function
+    printf("***********************************\n");
     assert(mylib::add(2, 3) == 5);
     assert(mylib::add(-1, 1) == 0);
     assert(mylib::add(0, 0) == 0);
+    assert(2==3);
     return 0;
 }
